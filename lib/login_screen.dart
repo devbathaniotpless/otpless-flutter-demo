f8bf7bf80763d72379759d3b3113b77d13186f46
devbathaniotpless <dev.bathani@otpless.com>
2024-01-04 15:45:32 +0530

    update: otpless integrated, login uri demo added

---
+++ lib/login_screen.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:otpless_flutter/otpless_flutter.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String? token;
+
+  ///Define the instance
+  final _otplessFlutterPlugin = Otpless();
+  var extra = {
+    "method": "get",
+    "params": {
+      "cid":
+          "HRIRBIIKXMKEOTDDA8VV4HP2V24454X8", //Replace the cid value with your CID value which is provided in the docs
+      "login_uri": Platform.isIOS
+          ? "com.flutter.app.otpless://otpless"
+          : "com.flutter.app", // Add this code to override the invalid package id
+    },
+  };
+
+  @override
+  void initState() {
+    super.initState();
+
+    //******************************************************** */
+    //This function will tell if WhatsApp is Installed or not
+    //******************************************************** */
+
+    _otplessFlutterPlugin.isWhatsAppInstalled().then(
+      (value) {
+        if (!value) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text("Please install the whatsapp"),
+              backgroundColor: Theme.of(context).hoverColor,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  //************************************************* */
+  //This function will run the login page in the app
+  //************************************************* */
+
+  Future<void> startOtpless() async {
+    await _otplessFlutterPlugin.hideFabButton();
+    _otplessFlutterPlugin.openLoginPage((result) {
+      if (result['data'] != null) {
+        // todo send this token to your backend service to validate otplessUser details received in the callback with OTPless backend service
+        token = result['data']['token'];
+        setState(() {});
+      }
+    }, jsonObject: extra);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Token : ",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    "$token",
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 100),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    startOtpless();
+                  },
+                  child: const Text("Login"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
