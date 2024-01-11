1126749a5e648fb6d6497bc3df9a426d032a2b0b
devbathani <bathanid888@gmail.com>
2024-01-11 17:19:33 +0530

    update: appdelegate and readme updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -12,8 +12,10 @@ import OtplessSDK
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    if Otpless.sharedInstance.isOtplessDeeplink(url: url){ Otpless.sharedInstance.processOtplessDeeplink(url: url)
+     return true
+    }
     super.application(app, open: url, options: options)
-    if Otpless.sharedInstance.isOtplessDeeplink(url: url){ Otpless.sharedInstance.processOtplessDeeplink(url: url) } 
     return true
-  } 
+  }
 }
