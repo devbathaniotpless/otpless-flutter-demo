62f9d6f77837b234a91ed6cbfe5169633b48d57b
devbathani <bathanid888@gmail.com>
2024-01-24 13:07:58 +0530

    fix: font size

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -80,7 +80,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   Text(
                     "$token",
                     style: const TextStyle(
-                      fontSize: 15,
+                      fontSize: 12,
                     ),
                   ),
                 ],
