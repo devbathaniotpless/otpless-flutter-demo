f3b1120220d1812adfe149c91cedb747b75dcbd8
devbathani <bathanid888@gmail.com>
2024-04-04 12:30:52 +0530

    update: otpless_flutter:2.1.1

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -13,51 +13,22 @@ class _LoginScreenState extends State<LoginScreen> {
 
   ///Define the instance
   final _otplessFlutterPlugin = Otpless();
-  var extra = {
-    "method": "get",
-    "params": {
-      "cid":
-          "HRIRBIIKXMKEOTDDA8VV4HP2V24454X8", //Replace the cid value with your CID value which is provided in the docs
-      "appId":
-          "ALP5OU9SMLB3NSPYGNSG" //Replace the appId value with your appId value which is provided in the docs
-    },
-  };
-
-  @override
-  void initState() {
-    super.initState();
-
-    //******************************************************** */
-    //This function will tell if WhatsApp is Installed or not
-    //******************************************************** */
-
-    _otplessFlutterPlugin.isWhatsAppInstalled().then(
-      (value) {
-        if (!value) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: const Text("Please install the whatsapp"),
-              backgroundColor: Theme.of(context).hoverColor,
-            ),
-          );
-        }
-      },
-    );
-  }
+  Map<String, dynamic> extra = {
+    'appId': "ALP5OU9SMLB3NSPYGNSG"
+  }; //Replace the appId value with your appId value which is provided in the docs
 
   //************************************************* */
   //This function will run the login page in the app
   //************************************************* */
 
   Future<void> startOtpless() async {
-    await _otplessFlutterPlugin.hideFabButton();
     _otplessFlutterPlugin.openLoginPage((result) {
       if (result['data'] != null) {
         // todo send this token to your backend service to validate otplessUser details received in the callback with OTPless backend service
         token = result['data']['token'];
         setState(() {});
       }
-    }, jsonObject: extra);
+    }, extra);
   }
 
   @override
