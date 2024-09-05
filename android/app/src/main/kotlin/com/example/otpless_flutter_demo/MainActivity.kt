823837472bc9bde7f5835ec3d03efbd828d3f4a9
devbathani <bathanid888@gmail.com>
2024-09-05 12:33:58 +0530

    update: base code

--- android/app/src/main/kotlin/com/example/otpless_flutter_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/otpless_flutter_demo/MainActivity.kt
@@ -1,17 +1,5 @@
 package com.example.otpless_flutter_demo
 
 import io.flutter.embedding.android.FlutterActivity
-import com.otpless.otplessflutter.OtplessFlutterPlugin;
-import android.content.Intent;
 
-class MainActivity: FlutterActivity(){
-    
-    override fun onBackPressed() {
-        val plugin = flutterEngine?.plugins?.get(OtplessFlutterPlugin::class.java)
-        if (plugin is OtplessFlutterPlugin) {
-        if (plugin.onBackPressed()) return
-        }
-        // handle other cases
-        super.onBackPressed()
-    }
-}
+class MainActivity: FlutterActivity()
